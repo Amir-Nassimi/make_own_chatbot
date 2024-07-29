dfabb6b4b391c8612648890d08f4a4a95225fcd0
a.nasimi <>
2024-07-29 13:23:12 +0330

    Modify login viewset

--- Chatbot_Project/Auth/views.py
+++ Chatbot_Project/Auth/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+
+class AuthViewSet(ViewSet):
+    permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            serializer = TokenObtainPairSerializer(data=request.data)
+            if serializer.is_valid():
+                return Response(serializer.validated_data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+       

09241f9b541528f628c4a65dee171225c5606311
a.nasimi <>
2024-07-29 13:49:52 +0330

    add Clients app setting to main project

--- Chatbot_Project/Chatbot_Project/settings.py
+++ Chatbot_Project/Chatbot_Project/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 
-    'Auth.apps.AuthConfig'
+    'Auth.apps.AuthConfig',
+    'Clients.apps.ClientsConfig'
 ]
 
 MIDDLEWARE = [

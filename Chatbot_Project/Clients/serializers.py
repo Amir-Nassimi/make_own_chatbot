95e0024063bc1b7bcd16fcfe41efcf4bb05e91f3
a.nasimi <>
2024-07-29 13:55:30 +0330

    Modify serializers and its source codes

---
+++ Chatbot_Project/Clients/serializers.py
@@ -0,0 +1,32 @@
+from django.db import transaction, IntegrityError
+
+from rest_framework.serializers import  ModelSerializer, ReadOnlyField, ValidationError
+
+from .models import Users
+
+
+
+class UserSerializer(ModelSerializer):
+    Joined = ReadOnlyField()
+
+    class Meta:
+        model = Users
+        fields = '__all__'
+
+
+class RegisterSerializer(ModelSerializer):
+
+    class Meta:
+        model = Users
+        fields = ['email', 'password', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def create(self, validated_data):
+        try:
+            with transaction.atomic():
+                user = Users.objects.create_user(**validated_data)
+
+                return user
+        except IntegrityError:
+            raise ValidationError("There was an error creating the users. Please try again.")

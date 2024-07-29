9eac62687a8d2c82a53a5bb3c2d15159f9904e4c
a.nasimi <>
2024-07-29 14:11:48 +0330

    Add urls for Clients - Modify Clients urls in main project urls

--- Chatbot_Project/Chatbot_Project/urls.py
+++ Chatbot_Project/Chatbot_Project/urls.py
@@ -26,6 +26,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('Auth.urls')),
+    path('user/', include('Clients.urls')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 
---
+++ Chatbot_Project/Clients/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from .views import ClientViewSet
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Chatbot Project - Client APIS",
+      default_version='v1',
+      description="This app is designed to manage the requests on clients",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="nassimiamir@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+router = DefaultRouter()
+router.register(r'', ClientViewSet, basename='client')
+
+urlpatterns = router.urls
+urlpatterns += [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+]

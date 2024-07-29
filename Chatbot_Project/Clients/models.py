78ad62311fc4d4397d925ba0bf4c83ea569fcbd1
a.nasimi <>
2024-07-29 13:48:33 +0330

    modify Users table

--- Chatbot_Project/Clients/models.py
+++ Chatbot_Project/Clients/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from uuid import uuid4 as UUID
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from Auth.models import CustomUserManager
+
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(default=UUID, primary_key=True, unique=True, editable=False)
+
+    last_name = models.TextField()
+    first_name = models.TextField()
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.email}'
+
+    @property
+    def Joined(self):
+        return self.date_joined.strftime("%Y/%m/%d %H:%M:%S")

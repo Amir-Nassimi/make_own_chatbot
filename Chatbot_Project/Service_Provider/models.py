1c8efa35a71fb2e60e095591ca245d933b1b7baa
a.nasimi <>
2024-08-05 22:35:28 +0330

    Add new fields and alter topics to be unique

--- Chatbot_Project/Service_Provider/models.py
+++ Chatbot_Project/Service_Provider/models.py
@@ -22,6 +22,7 @@ class TrainableData(models.Model):
 
     answers = models.TextField()
     questions = models.TextField()
-    topic = models.CharField(max_length=20)
+    used = models.BooleanField(default=False)
+    topic = models.CharField(max_length=20, unique=True)
 
     bot = models.ForeignKey(ChatBot, on_delete=models.CASCADE, related_name='Chatbot_TrainableData')

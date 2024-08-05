15fe7764399cba37ecb809979f2cb3abb6f02e40
a.nasimi <>
2024-08-05 20:56:37 +0330

    modify modelveiwset to create trainable's

--- Chatbot_Project/Service_Provider/views.py
+++ Chatbot_Project/Service_Provider/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import TrainableData
+from .serializers import TrainableDataSerializer
+
+
+
+class TrainableDataViewSet(ModelViewSet):
+    queryset = TrainableData.objects.al()
+    serializer_class = TrainableDataSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    

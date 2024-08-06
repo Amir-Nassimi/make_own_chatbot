f36ac34730c298d4a69666f2726ec4c472eb843a
a.nasimi <>
2024-08-06 10:41:26 +0330

    Creat Config file - Modify Path for storing rasa data

---
+++ Chatbot_Project/conf.py
@@ -0,0 +1,12 @@
+from singleton_decorator import singleton
+
+
+
+@singleton
+class Config:
+    def __init__(self):
+        pass
+    
+    @property
+    def Path(bot_id):
+        return f'RasaData/{bot_id}'

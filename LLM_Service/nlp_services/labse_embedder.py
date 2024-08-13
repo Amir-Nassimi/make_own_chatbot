371433b910981c328eaa78263338a3baaad7d37b
a.nasimi <>
2024-08-13 19:55:19 +0330

    Optimize the method of adding features

--- LLM_Service/nlp_services/labse_embedder.py
+++ LLM_Service/nlp_services/labse_embedder.py
@@ -1,58 +1,71 @@
 import requests
+import numpy as np
 from typing import Any, Dict, List, Text
 
 from rasa.shared.nlu.constants import TEXT
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
 from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.training_data.features import Features
 from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.graph import GraphComponent, ExecutionContext
 from rasa.shared.nlu.training_data.training_data import TrainingData
 
 
 
-
 @DefaultV1Recipe.register(
-    DefaultV1Recipe.ComponentType.MESSAGE_FEATURIZER,
-    is_trainable=False
+    DefaultV1Recipe.ComponentType.MESSAGE_FEATURIZER, is_trainable=False
 )
 class LaBSEEmbedder(GraphComponent):
-    api_url = ''
+    api_url = ""
+
     def __init__(self, config: Dict[Text, Any]) -> None:
         self.api_url = config.get("api_url", None)
 
     @classmethod
     def get_default_config(cls) -> Dict[Text, Any]:
-        return {
-            "api_url": cls.api_url
-        }
+        return {"api_url": cls.api_url}
 
     def process(self, messages: List[Message]) -> List[Message]:
         for message in messages:
             text = message.get(TEXT)
 
             if text is None:
-                continue 
-            
+                continue
+
             embeddings = self._get_embeddings(text)
-            message.set("Featurizer", embeddings)
+            feature = Features(
+                features=embeddings,
+                feature_type="sequence",
+                origin="LaBSEEmbedder",
+                attribute=TEXT,
+            )
+
+            message.add_features(feature)
         return messages
 
     def process_training_data(self, training_data: TrainingData) -> TrainingData:
         for message in training_data.training_examples:
             text = message.get(TEXT)
             embeddings = self._get_embeddings(text)
-            message.set("Featurizer", embeddings)
+            feature = Features(
+                features=embeddings,
+                feature_type="sequence",
+                origin="LaBSEEmbedder",
+                attribute=TEXT,
+            )
+            message.add_features(feature)
 
         return training_data
 
     def _get_embeddings(self, text: str) -> List[float]:
-        response = requests.post(
-            self.api_url,
-            json={"text": [text]}
-        )
+        response = requests.post(self.api_url, json={"text": [text]})
         if response.status_code == 200:
-            return response.json()['embeddings'][0]
+            embeddings =  response.json()["embeddings"][0]
+            embeddings_array = np.array(embeddings)
+            if embeddings_array.ndim == 1:
+                embeddings_array = embeddings_array.reshape(1, -1)
+            return embeddings_array
         else:
             raise ValueError(f"Error from LaBSE API: {response.text}")
 

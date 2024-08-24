cf9d97c555650624aafc7947bb5a7904a3831b58
a.nasimi <>
2024-08-24 13:42:02 +0430

    reModify the Embeddings output

--- LLM_Service/nlp_services/labse_embedder.py
+++ LLM_Service/nlp_services/labse_embedder.py
@@ -1,14 +1,14 @@
-import requests
-import numpy as np
 from typing import Any, Dict, List, Text
 
-from rasa.shared.nlu.constants import TEXT
+import numpy as np
+import requests
+from rasa.engine.graph import ExecutionContext, GraphComponent
+from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
-from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.constants import TEXT
 from rasa.shared.nlu.training_data.features import Features
-from rasa.engine.recipes.default_recipe import DefaultV1Recipe
-from rasa.engine.graph import GraphComponent, ExecutionContext
+from rasa.shared.nlu.training_data.message import Message
 from rasa.shared.nlu.training_data.training_data import TrainingData
 
 
@@ -30,9 +30,6 @@ class LaBSEEmbedder(GraphComponent):
         for message in messages:
             text = message.get(TEXT)
 
-            if text is None:
-                continue
-
             embeddings = self._get_embeddings(text)
             feature = Features(
                 features=embeddings,
@@ -40,13 +37,13 @@ class LaBSEEmbedder(GraphComponent):
                 origin="LaBSEEmbedder",
                 attribute=TEXT,
             )
-
             message.add_features(feature)
         return messages
 
     def process_training_data(self, training_data: TrainingData) -> TrainingData:
         for message in training_data.training_examples:
             text = message.get(TEXT)
+
             embeddings = self._get_embeddings(text)
             feature = Features(
                 features=embeddings,
@@ -61,11 +58,9 @@ class LaBSEEmbedder(GraphComponent):
     def _get_embeddings(self, text: str) -> List[float]:
         response = requests.post(self.api_url, json={"text": [text]})
         if response.status_code == 200:
-            embeddings =  response.json()["embeddings"][0]
-            embeddings_array = np.array(embeddings)
-            if embeddings_array.ndim == 1:
-                embeddings_array = embeddings_array.reshape(1, -1)
-            return embeddings_array
+            embeddings = response.json()["embeddings"]
+            embeddings_array = np.array(embeddings).squeeze()
+            return embeddings_array[1:-1] #Avoid [CLS] Token and [SEP] Token
         else:
             raise ValueError(f"Error from LaBSE API: {response.text}")
 

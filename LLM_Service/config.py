b0642ed8efe1890b074aa4487a666c4be3bfdd7d
a.nasimi <>
2024-08-24 13:36:24 +0430

    Modify Tokenizer - Set Normalization to False

--- LLM_Service/config.py
+++ LLM_Service/config.py
@@ -1,17 +1,21 @@
 from singleton_decorator import singleton
 
 
-
 @singleton
 class Config:
     def __init__(self):
-        self._NLP_Normalization = True
-        self._NLP_Model = 'sentence-transformers/LaBSE'
-    
+        self._NLP_Normalization = False
+        self._NLP_Tokenizer = "token_embeddings"
+        self._NLP_Model = "sentence-transformers/LaBSE"
+
     @property
     def NLP_Model(self):
         return self._NLP_Model
     
+    @property
+    def NLP_Tokenizer(self):
+        return self._NLP_Tokenizer
+
     @property
     def NLP_Normalization(self):
         return self._NLP_Normalization
